f737917049244fb3dda63b1faf657bdf306f6339
Arvind <thakurarvindkr10@gmail.com>
2023-08-05 19:17:03 +0530

    added plans what to do

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import CssBaseline from '@mui/material/CssBaseline';
+import { Routes, Route } from 'react-router-dom';
 const App = () => {
     return (
         <div>
-            hello World
+            <CssBaseline />
+            <main>
+                <Routes>
+                    <Route path='/' element={<h2>Movies</h2>} />
+                    <Route path='/movie/:id' element={<h2>Movie Information</h2>} />
+                    <Route path='/actors/:id' element={<h2>Actors</h2>} />
+                    <Route path='/profile/:id' element={<h2>Profile</h2>} />
+                </Routes>
+            </main>
         </div>
     )
 }
