88506fd1c75314e047bc120e2b029267b4e00d26
Arvind <thakurarvindkr10@gmail.com>
2023-08-05 20:02:09 +0530

    added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import { Routes, Route } from 'react-router-dom';
+import { Movies, Profile, Actors, MovieInformation, Navbar } from '../components/index'
+
 const App = () => {
     return (
         <div>
             <CssBaseline />
+            <Navbar />
             <main>
                 <Routes>
-                    <Route path='/' element={<h2>Movies</h2>} />
-                    <Route path='/movie/:id' element={<h2>Movie Information</h2>} />
-                    <Route path='/actors/:id' element={<h2>Actors</h2>} />
-                    <Route path='/profile/:id' element={<h2>Profile</h2>} />
+                    <Route path='/' element={<Movies />} />
+                    <Route path='/movie/:id' element={<MovieInformation />} />
+                    <Route path='/actors/:id' element={<Actors />} />
+                    <Route path='/profile/:id' element={<Profile />} />
                 </Routes>
             </main>
         </div>
